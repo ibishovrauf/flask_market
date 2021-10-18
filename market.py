b98f36f7c6d50e4db14262c9db6d0ffabb7509cf
ibisovrauf <raufibis0v666@gmail.com>
2021-10-18 16:59:58 +0400

    hj

--- market.py
+++ market.py
@@ -1,9 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from sqlalchemy import create_engine
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:passmysql@localhost/market'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/hhhhh'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -13,15 +13,43 @@ class Item(db.Model):
     name = db.Column(db.String(length=30), nullable=False, unique=True)
     price = db.Column(db.Integer(), nullable=False)
     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
-    description = db.Column(db.String(length=1024), nullable=False, unique=True)
+    description = db.Column(db.String(length=124), nullable=False, unique=True)
 
 
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    firstname = db.Column(db.String(length=30), nullable=False)
+    lastname = db.Column(db.String(length=30), nullable=False)
+    email = db.Column(db.String(length=50), nullable=False)
+    password = db.Column(db.String(length=30), nullable=False)
 
 
 @app.route('/')
 @app.route('/home')
 def home_page():
     return render_template('home.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/sign-up', methods=['POST', 'GET'])
+def sign_up():
+    if request.method == "POST":
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        password = request.form['password']
+        user = User(firstname=firstname, lastname=lastname, email=email, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return render_template('register.html')
+    else:
+        return render_template('register.html')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
