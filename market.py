79301bfb4d7d489c7c4d1f1288163b78335305ca
ibisovrauf <raufibis0v666@gmail.com>
2021-10-18 14:04:51 +0400

    hj

---
+++ market.py
@@ -0,0 +1,28 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template
+from sqlalchemy import create_engine
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:passmysql@localhost/market'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
+
+
+
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('test.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
