aaeeca1da2f41d013f7104cf60cbbfeecc9548b1
ibisovrauf <raufibis0v666@gmail.com>
2021-10-22 21:05:47 +0400

    test

--- market.py
+++ market.py
@@ -1,20 +1,36 @@
+import os
+
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-
-
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:passmysql@localhost/market'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:passmysql@localhost/market'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/market'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "test123test"
 
 db = SQLAlchemy(app)
 admin = Admin(app)
+Base = declarative_base()
 
 
+class Parent(Base):
+    __tablename__ = 'parent'
+    id = db.Column(db.Integer, primary_key=True)
+    child_id = db.Column(db.Integer, ForeignKey('child.id'))
+    child = relationship("Child", back_populates="parents")
 
+
+class Child(Base):
+    __tablename__ = 'child'
+    id = db.Column(db.Integer, primary_key=True)
+    parents = relationship("Parent", back_populates="child")
 
 class Item(db.Model):
     __tablename__ = 'item'
@@ -23,7 +39,9 @@ class Item(db.Model):
     price = db.Column(db.Integer(), nullable=False)
     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
     description = db.Column(db.String(length=124), nullable=False, unique=True)
-
+    user_id = db.Column(db.Integer(), ForeignKey("user.id"), nullable = False)
+    user = relationship("User", back_populates="item")
+    basket = relationship("Basket", back_populates="item")
 
 class User(db.Model):
     __tablename__ = 'user'
@@ -31,11 +49,30 @@ class User(db.Model):
     firstname = db.Column(db.String(length=30), nullable=False)
     lastname = db.Column(db.String(length=30), nullable=False)
     email = db.Column(db.String(length=50), nullable=False)
-    password = db.Column(db.String(length=30), nullable=False)
+    password = db.Column(db.String(length=124), nullable=False)
+    item = relationship("Item", back_populates="user")
+    basket = relationship("Basket", back_populates="user")
+
+
+class Basket(db.Model):
+    __tablename__ = 'basket'
+    id = db.Column(db.Integer(), primary_key=True)
+    user_basket_id = db.Column(db.Integer(), ForeignKey("user.id"))
+    item_basket_id = db.Column(db.Integer(), ForeignKey("item.id"))
+    user = relationship("User", back_populates="basket")
+    item = relationship("Item", back_populates="basket")
+
+
+#
+# class Basket(db.Model):
+#     id = db.Column(db.Integer(), primary_key=True)
+#     user_id = db.Column(db.Integer(), ForeignKey('User.id'))
+#     item_id = db.Column(db.Integer(), ForeignKey('Item.id'))
+
 
 admin.add_view(ModelView(User, db.session))
 admin.add_view(ModelView(Item, db.session))
-
+admin.add_view(ModelView(Basket, db.session))
 
 
 @app.route('/')
@@ -46,25 +83,35 @@ def home_page():
 
 @app.route('/login')
 def login():
+
     return render_template('login.html')
 
 
-@app.route('/sign-up', methods=['POST', 'GET'])
+@app.route('/sign-up', methods=['POST'])
 def sign_up():
-    if request.method == "POST":
-        firstname = request.form['firstname']
-        lastname = request.form['lastname']
-        email = request.form['email']
-        password = request.form['password']
-        user = User(firstname=firstname, lastname=lastname, email=email, password=password)
-        db.session.add(user)
-        db.session.commit()
-        return render_template('register.html')
-    else:
-        return render_template('register.html')
+    firstname = request.form['firstname']
+    lastname = request.form['lastname']
+    email = request.form['email']
+    password = request.form['password']
 
+    hashed = generate_password_hash(password, method="sha256")
+    user = User(firstname=firstname, lastname=lastname, email=email, password=hashed)
+
+    db.session.add(user)
+    db.session.commit()
+    return render_template('home.html')
+
+
+@app.route('/sign-up', methods=['GET'])
+def sign_up_get():
+    return render_template('register.html')
+
+
+# cloudinary.config(
+#     cloud_name=os.getenv('rauf2'),
+#     api_key=os.getenv('494188146737927'),
+#     api_secret=os.getenv('73oXW8mcSLuJoxZdWxcaItSM-rY'))
 
 if __name__ == "__main__":
-
     app.run(debug=True)
 
