dc1aaa38bfc1699179532350b4ab5e7fae26e592
ibisovrauf <raufibis0v666@gmail.com>
2021-12-06 07:40:18 +0400

    search

--- market.py
+++ market.py
@@ -1,3 +1,4 @@
+"""Docstring"""
 import cloudinary
 import cloudinary.uploader
 
@@ -10,6 +11,7 @@ from flask_admin.contrib.sqla import ModelView
 from sqlalchemy import ForeignKey, Integer, Column, String, Boolean
 from sqlalchemy.orm import relationship, backref
 from werkzeug.security import generate_password_hash, check_password_hash
+import simhash
 import clodinary_config
 
 app = Flask(__name__)
@@ -21,7 +23,6 @@ login_manager = LoginManager()
 login_manager.login_view = 'login'
 login_manager.init_app(app)
 db = SQLAlchemy(app)
-
 
 cloudinary.config(
     cloud_name=clodinary_config.cloud_name,
@@ -74,11 +75,21 @@ class Basket(db.Model):
     items = relationship("Item")
 
 
+class BasketView(ModelView):
+    """docstring"""
+    can_delete = False  # disable user  deletion
+    can_edit = False  # disable user  edition
+    can_create = False  # disable user creation
+    column_list = ['users', 'items']
+
+
 class ItemView(ModelView):
+    """docstring"""
     edit_template = 'edit_item.html'
     create_template = 'create_item.html'
     can_delete = False  # disable model deletion
-    page_size = 10  #
+    can_edit = False  # disable model edition
+    page_size = 10
     column_exclude_list = ['photo', ]
 
     @expose('/new/', methods=['POST'])
@@ -86,21 +97,22 @@ class ItemView(ModelView):
         """
             Custom create view.
         """
+
         item_name = request.form['ItemName']
         price = request.form['Price']
         barcode = request.form['Barcode']
         description = request.form['Description']
-
         file = request.files['file']
         upload_result = cloudinary.uploader.upload(file)
         photo_url = upload_result["secure_url"]
-
         item = Item(name=item_name, price=price, barcode=barcode, description=description,
                     photo=photo_url, user_id=current_user.id)
         item__name = Item.query.filter_by(name=item_name, user_id=current_user.id).first()
         barcode_1 = Item.query.filter_by(barcode=barcode).first()
         db.session.add(item)
         db.session.commit()
+        item_search = Item.query.filter_by(barcode=barcode).first()
+
         if item__name:
             flash('Item is already added', category='error')
         elif barcode_1:
@@ -112,16 +124,19 @@ class ItemView(ModelView):
 
     @expose('/new/', methods=['GET'])
     def create_view_get(self):
+        """Docstring"""
         return self.render('create_item.html')
 
 
 class UserView(ModelView):
+    """docstring"""
     create_template = "create_user.html"
-    page_size = 10  #
+    page_size = 10
     column_exclude_list = ['photo', 'password', ]
 
     @expose('/new/', methods=["POST"])
     def create_app(self):
+        """docstring"""
         enter = request.form.get('admin')
         if enter == "admin":
             admin_o = True
@@ -150,34 +165,43 @@ class UserView(ModelView):
             db.session.add(user)
             db.session.commit()
             user_id = user.id
-            basket = Basket(basket_user_id=user_id)
-            db.session.add(basket)
-            db.session.commit()
+            if admin_o is False:
+                user_cart = Basket(basket_user_id=user_id)
+                db.session.add(user_cart)
+                db.session.commit()
             login_user(user, remember=True)
             flash('Account created!', category='success')
         return self.render('create_user.html')
 
     @expose('/new/', methods=["GET"])
     def create_new_user_get(self):
+        """docstring"""
         return self.render('create_user.html')
 
 
 class MyAdminIndexView(AdminIndexView):
+    """docstring"""
     def is_accessible(self):
-        if current_user.admin == True:
+        """docstring"""
+        if current_user.admin is True:
             return current_user.is_authenticated
+        return
 
 
 admin = Admin(app, index_view=MyAdminIndexView())
 admin.add_view(UserView(User, db.session))
 admin.add_view(ItemView(Item, db.session))
-admin.add_view(ModelView(Basket, db.session))
+admin.add_view(BasketView(Basket, db.session))
 
 
 @app.route('/')
 @app.route('/home')
 def home_page():
     """docstring"""
+    hash0 = simhash.Simhash("facebook at ufaz")
+    hash1 = simhash.Simhash("facebok at ufaz")
+    hash2 = simhash.Simhash("facəook at ufaz")
+    print(hash0, hash1, hash2)
     return render_template('home.html')
 
 
@@ -210,8 +234,8 @@ def sign_up():
         db.session.add(user)
         db.session.commit()
         user_id = user.id
-        bascket = Basket(basket_user_id=user_id)
-        db.session.add(bascket)
+        user_cart = Basket(basket_user_id=user_id)
+        db.session.add(user_cart)
         db.session.commit()
         login_user(user, remember=True)
         flash('Account created!', category='success')
@@ -281,20 +305,11 @@ def show_item(item_id):
     return render_template('item.html', item=item, user=user)
 
 
-@app.route('/items', methods=['GET'])
-def all_items():
-    """docstring"""
-    page = request.args.get('page', 1, type=int)
-    items = Item.query.paginate(page=page, per_page=6)
-    return render_template('items_page.html', items=items)
-
-
-@app.route('/items', methods=['POST'])
-def show_item_post():
+@app.route('/items/<item_id>', methods=['POST'])
+def show_item_post(item_id):
     """docstring"""
     if current_user.is_authenticated:
-        item = request.form['item']
-        item = Item.query.filter_by(id=item).first()
+        item = Item.query.filter_by(id=item_id).first()
         user_cart = Basket.query.filter_by(basket_user_id=current_user.id).first()
         item_is_exist = 0
         for i in user_cart.items:
@@ -307,8 +322,7 @@ def show_item_post():
         else:
             user_cart.items.append(item)
             db.session.commit()
-
-        return redirect(url_for('all_items'))
+        return redirect(url_for('show_item', item_id=item_id))
     flash("First you must login", category="error")
     return redirect(url_for('login'))
 
@@ -316,7 +330,7 @@ def show_item_post():
 @app.route('/basket', methods=['GET'])
 def basket():
     """docstring"""
-    if current_user.admin == True:
+    if current_user.admin is True:
         return redirect(url_for('profile'))
     user_cart = Basket.query.filter_by(basket_user_id=current_user.id).first()
     item = user_cart.items
@@ -348,22 +362,19 @@ def redact_profile():
 @app.route('/profile/redact-profile', methods=['POST'])
 def redact_profile_post():
     """docstring"""
-
     first_name = request.form['firstname']
     lastname = request.form['lastname']
     username = request.form['username']
-
     file = request.files['file']
-    upload_result = cloudinary.uploader.upload(file)
-    photo_url = upload_result["secure_url"]
-
     if first_name != current_user.firstname:
-        current_user.firstname = first_name
+        db.session.current_user.firstname = first_name
     if lastname != current_user.lastname:
         current_user.lastname = lastname
     if username != current_user.username:
         current_user.username = username
-    if file:
+    if file.filename != "":
+        upload_result = cloudinary.uploader.upload(file)
+        photo_url = upload_result["secure_url"]
         current_user.photo = photo_url
     db.session.commit()
     return redirect(url_for('profile'))
@@ -372,22 +383,119 @@ def redact_profile_post():
 @app.route('/profile/my-items')
 def my_items():
     """docstring"""
-    items = Item.query.filter_by(user_id=current_user.id).all()
-    # print(items)
-    return render_template("users_item.html", item=items)
+    if current_user.admin is True:
+        page = request.args.get('page', 1, type=int)
+        items = Item.query.filter_by(user_id=current_user.id).paginate(page=page, per_page=3)
+
+        return render_template("users_item.html", item=items)
+    return render_template("404.html")
+
+
+@app.route('/my_items/search', methods=['POST'])
+def my_items_search():
+    """docstring"""
+    search1 = request.form["search"]
+    if not search1:
+        search1 = "wrong search"
+    order1 = "up_price"
+    return redirect(url_for('my_items_search_results', query=search1, order=order1))
+
+
+@app.route('/search/vs', methods=['POST'])
+def search_order_my_items():
+    """docstring"""
+    value = request.form.get('thing')
+    val = value.split(",")
+    return redirect(url_for('my_items_search_results', query=val[1], order=val[0]))
+
+
+@app.route('/my_items/<query>/<order>')
+def my_items_search_results(query, order):
+    """docstring"""
+    page = request.args.get('page', 1, type=int)
+    if order == "up_price":
+        items = Item.query.order_by(Item.price.desc())\
+                          .filter(Item.name.contains(query) | Item.description.contains(query))\
+                          .filter_by(user_id=current_user.id)\
+                          .paginate(page=page, per_page=3)
+    elif order == "barcode":
+        items = Item.query.order_by(Item.barcode.desc()) \
+                          .filter(Item.name.contains(query) | Item.description.contains(query))\
+                          .filter_by(user_id=current_user.id)\
+                          .paginate(page=page, per_page=3)
+    elif order == "name":
+        items = Item.query.order_by(Item.name.desc())\
+            .filter(Item.name.contains(query) | Item.description.contains(query)) \
+            .filter_by(user_id=current_user.id)\
+            .paginate(page=page, per_page=3)
+    return render_template('users_item_result.html', items=items, query=query, thing=order)
+
+
+@app.route('/items', methods=['GET'])
+def all_items():
+    """docstring"""
+    page = request.args.get('page', 1, type=int)
+    items = Item.query.paginate(page=page, per_page=3)
+    return render_template('items_page.html', items=items)
+
+
+@app.route('/items/order-by', methods=['POST'])
+def ordered_items():
+    """docstring"""
+    value = request.form.get('thing')
+    page = request.args.get('page', 1, type=int)
+    if value == "name":
+        items = Item.query.order_by(Item.name.desc()).paginate(page=page, per_page=3)
+    elif value == "barcode":
+        items = Item.query.order_by(Item.barcode.desc()).paginate(page=page, per_page=3)
+    elif value == "up_price":
+        items = Item.query.order_by(Item.price.desc()).paginate(page=page, per_page=3)
+    return render_template('items_page.html', items=items, thing=value)
+
+
+@app.route('/search/v', methods=['POST'])
+def search_order():
+    """docstring"""
+    value = request.form.get('thing')
+    val = value.split(",")
+    return redirect(url_for('search_results', query=val[1], order=val[0]))
 
 
 @app.route('/search', methods=['POST'])
 def search():
+    """docstring"""
     search1 = request.form["search"]
-    return redirect((url_for('search_results', query=search1)))
+    if not search1:
+        search1 = "wrong search"
+    order1 = "up_price"
+    return redirect(url_for('search_results', query=search1, order=order1))
 
 
-@app.route('/search_results/<query>')
-def search_results(query):
-    results = Item.query.filter(Item.name.contains(query) | Item.description.contains(query)).all()
-    print(results)
-    return render_template('search_results.html', query=query, results=results)
+@app.route('/search_results/<query>/<order>')
+def search_results(query, order):
+    """docstring"""
+    page = request.args.get('page', 1, type=int)
+    if order == "up_price":
+        items = Item.query.order_by(Item.price.desc())\
+                          .filter(Item.name.contains(query) | Item.description.contains(query))\
+                          .paginate(page=page, per_page=3)
+    elif order == "barcode":
+        items = Item.query.order_by(Item.barcode.desc()) \
+                          .filter(Item.name.contains(query) | Item.description.contains(query)) \
+                          .paginate(page=page, per_page=3)
+    elif order == "name":
+        items = Item.query.order_by(Item.name.desc()) \
+            .filter(Item.name.contains(query) | Item.description.contains(query)) \
+            .paginate(page=page, per_page=3)
+    return render_template('search_results.html', items=items, query=query, thing=order)
+
+
+@app.route('/delete/<item_id>', methods=['POST'])
+def delete_item(item_id):
+    item = Item.query.filter_by(id=item_id).first()
+    db.session.delete(item)
+    db.session.commit()
+    return redirect(url_for('home_page'))
 
 
 if __name__ == "__main__":
